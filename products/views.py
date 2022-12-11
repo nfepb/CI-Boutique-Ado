d41e0607d01ad763502e8009a65e3051dfd1849e
nfepb <nicolas.planteb@gmail.com>
2022-12-11 18:20:51 +0000

    Fixes view variable for category for all_products.

--- products/views.py
+++ products/views.py
@@ -12,7 +12,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
 
     """
     To match term in either product name or description through queries
