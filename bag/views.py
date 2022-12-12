a384e7eab081faa2763c6432843585ed0d369e84
nfepb <nicolas.planteb@gmail.com>
2022-12-12 17:46:41 +0000

    Creates bag.html file and updates views.py to render bag page.

--- bag/views.py
+++ bag/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def view_bag(request):
+    """
+    View to render the bag contents page
+    """
+    return render(request, 'bag/bag.html')
