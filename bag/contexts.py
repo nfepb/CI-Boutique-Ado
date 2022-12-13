bc967c936260ab71a2631ca9992e0053e15f20c9
nfepb <nicolas.planteb@gmail.com>
2022-12-13 19:49:30 +0000

    Updates bag to retrieve and return size with items in list.

--- bag/contexts.py
+++ bag/contexts.py
@@ -17,19 +17,34 @@ def bag_contents(request):
     bag = request.session.get('bag', {})
 
     # Bag item from the session
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        """
-        Adds dictionnary to list of bag items to give access to
-        other fields when iterating through itesmmseg. image)
-        """
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        # Check if item has a size by checking if int
+        # If int, we know int data is qty.
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            """
+            Adds dictionnary to list of bag items to give access to
+            other fields when iterating through itesmmseg. image)
+            """
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        # If not int, it is a dictionnary.
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
