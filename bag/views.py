dcafacddb3c9262226c8d376d67424353cb7b17b
nfepb <nicolas.planteb@gmail.com>
2022-12-15 18:35:49 +0000

    Adds adjust bag and delete from bag views in bag/. Updates the urls for redirect & reverse. Adds action to the items in form from bag.html. Issue with property in data library l.192 bag.html JS being changed automatically.

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def view_bag(request):
@@ -44,3 +44,67 @@ def add_to_bag(request, item_id):
 # Overwrites the variable in the session with updated version
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """
+    Adjusts the quantity of the specified products to the shopping bag
+    """
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+
+    # If there is a bag variable in the session, adds to dictionnary,
+    # if not, creates one
+    bag = request.session.get('bag', {})
+
+    if size:
+        # if item is already in bag:
+        if quantity > 0:
+            """
+            Drill items by size dictionnary,
+            find specific size, update qty
+            """
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            # Remove item if qty = 0
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            """
+            If qty is 0, removes the item
+            """
+            bag[item_id] = quantity
+        else:
+            # Remove item if qty = 0
+            bag.pop(item_id)
+
+    # Overwrites the variable in the session with updated version
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
