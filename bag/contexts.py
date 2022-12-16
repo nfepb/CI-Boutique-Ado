e6b9483d390437e92dbf64b36c6c1ec2e21c6e2d
nfepb <nicolas.planteb@gmail.com>
2022-12-16 14:14:09 +0000

    Adds functionality to add/remove qties from bag page cpying source CI code.

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,37 +3,23 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
-    """
-    Returns a dictionnary as context processor.
-    Makes the dictionnary available to all templates across
-    all the application.
-    """
 
     bag_items = []
     total = 0
     product_count = 0
     bag = request.session.get('bag', {})
 
-    # Bag item from the session
     for item_id, item_data in bag.items():
-        # Check if item has a size by checking if int
-        # If int, we know int data is qty.
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
             product_count += item_data
-            """
-            Adds dictionnary to list of bag items to give access to
-            other fields when iterating through itesmmseg. image)
-            """
             bag_items.append({
                 'item_id': item_id,
-                'quantity': quantity,
+                'quantity': item_data,
                 'product': product,
             })
-        # If not int, it is a dictionnary.
         else:
             product = get_object_or_404(Product, pk=item_id)
             for size, quantity in item_data['items_by_size'].items():
@@ -47,15 +33,14 @@ def bag_contents(request):
                 })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
-
+    
     grand_total = delivery + total
-
-# Make items available throughout the app
+    
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -1,88 +1,62 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
+# Create your views here.
 
 def view_bag(request):
-    """
-    View to render the bag contents page
-    """
+    """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
-
 def add_to_bag(request, item_id):
-    """
-    Add a quantity of the specified products to the shopping bag
-    """
+    """ Add a quantity of the specified product to the shopping bag """
+
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-
-    # If there is a bag variable in the session, adds to dictionnary,
-    # if not, creates one
     bag = request.session.get('bag', {})
 
     if size:
-        # if item is already in bag:
         if item_id in list(bag.keys()):
             if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
             else:
-                # If item already in bag but not in size:
                 bag[item_id]['items_by_size'][size] = quantity
-        # If not already in bag, will add item and list by size of items.
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
     else:
         if item_id in list(bag.keys()):
-            # Updates the quantity if already in bag
             bag[item_id] += quantity
         else:
-            # Adds item to the bag if not in bag
             bag[item_id] = quantity
 
-# Overwrites the variable in the session with updated version
     request.session['bag'] = bag
     return redirect(redirect_url)
-
+    
 
 def adjust_bag(request, item_id):
-    """
-    Adjusts the quantity of the specified products to the shopping bag
-    """
+    """Adjust the quantity of the specified product to the specified amount"""
+
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
-
-    # If there is a bag variable in the session, adds to dictionnary,
-    # if not, creates one
     bag = request.session.get('bag', {})
 
     if size:
-        # if item is already in bag:
         if quantity > 0:
-            """
-            Drill items by size dictionnary,
-            find specific size, update qty
-            """
             bag[item_id]['items_by_size'][size] = quantity
         else:
-            # Remove item if qty = 0
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
     else:
         if quantity > 0:
-            """
-            If qty is 0, removes the item
-            """
             bag[item_id] = quantity
         else:
-            # Remove item if qty = 0
             bag.pop(item_id)
 
-    # Overwrites the variable in the session with updated version
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
