91690b85e51041f6b27a46ebb10598b03dd04a68
nfepb <nicolas.planteb@gmail.com>
2022-12-23 12:04:13 +0000

    Updates stripe_elements.js to display error message real time during checkout.

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -22,3 +22,23 @@ const appearance = {
 let card = elements.create("card", { appearance: appearance });
 // Mount card on the div in checkout page
 card.mount("#card-element");
+
+// Handle real-time validation errors on the card element
+card.addEventListener("change", function (event) {
+  const errorDiv = document.getElementById("card-errors");
+  if (event.error) {
+    const html = `
+      <span class="icon" role="alert">
+        <i class="fa fa-times"></i>
+      </span>
+      <span>${event.error.message}</span>
+      `;
+    $(errorDiv).html(html);
+  } else {
+    errorDiv.textContent = "";
+  }
+});
+
+// STEP 1: Checkout view creates payment intent
+// STEP 2: Stripe returns unique client_secret that we return to template
+// STEP 3: Use client_secret in template to call confirmCardPayment() & verify the card
