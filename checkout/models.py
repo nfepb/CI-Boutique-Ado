ca1ea17687943d56e688ec1e4768e70c70aa2d04
nfepb <nicolas.planteb@gmail.com>
2022-12-28 17:39:45 +0000

    Fixes signals.py to allow deletion of order, updates models for line_items to allow modification in order from admin panel.

--- checkout/models.py
+++ checkout/models.py
@@ -77,7 +77,7 @@ class OrderLineItem(models.Model):
         Product, null=False, blank=False, on_delete=models.CASCADE
         )
     product_size = models.CharField(
-        max_length=2, null=False, blank=False
+        max_length=2, null=True, blank=True
         )  # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(
