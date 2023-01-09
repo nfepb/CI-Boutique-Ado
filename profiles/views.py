dd18bcfa4c1fa5b1014a9a7587a85d9430dac414
nfepb <nicolas.planteb@gmail.com>
2023-01-09 19:29:44 +0000

    fix: display success on updating profile without link to bag.

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -12,6 +13,7 @@ def profile(request):
 
     # When updating profile info
     if request.method == 'POST':
+        # instance of profile defined in method
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
@@ -25,6 +27,7 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
